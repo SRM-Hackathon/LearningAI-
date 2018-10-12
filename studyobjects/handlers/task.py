c770a46e8e9a9b2db09663ca060fcc1d70055c86
Panneer <panneer.selvam@entrayn.com>
2018-10-12 20:06:53 +0530

    list todo, inprogress and completed

--- studyobjects/handlers/task.py
+++ studyobjects/handlers/task.py
@@ -10,7 +10,9 @@ from utils import get_displaced_time_from_duration_entity, add_entity_in_dialogf
 
 
 class TaskHandler(IntentHandler):
-
+    def __init__(self, intent_response_dict, user, action):
+        super().__init__(intent_response_dict, user, action)
+        self.user_environment = UserEnvironment.objects.get(user=self.user)
 
     def get_object(self):
         course_name = self.response.get("course")
@@ -26,10 +28,9 @@ class TaskHandler(IntentHandler):
         name = self.response["name"]
         formatted_task_name = slugify(self.response["name"])
         eta = get_displaced_time_from_duration_entity(timezone.now(), self.response["eta"])
-        team = self.user.team
-        user_environment = UserEnvironment.objects.get(user=self.user)
-        assessment = user_environment.assessment
-        tag = user_environment.tag
+
+        assessment = self.user_environment.assessment
+        tag = self.user_environment.tag
         Task.objects.get_or_create(
             name=formatted_task_name,
             assessment=assessment,
@@ -39,3 +40,33 @@ class TaskHandler(IntentHandler):
         )
         add_entity_in_dialogflow("Task", formatted_task_name, [name, ])
         return True
+
+    def list_todo(self):
+        tasks = Task.objets.filter(
+            student=self.user,
+            state=Task.TODO,
+            assessment=self.user_environment.assessment,
+            tag=self.user_environment.tag
+        ).values_list('name', flat=True)
+        formatted_task_names = "{}".format("\n".join(tasks))
+        return formatted_task_names
+
+    def list_inprogress(self):
+        tasks = Task.objets.filter(
+            student=self.user,
+            state=Task.IN_PROGRESS,
+            assessment=self.user_environment.assessment,
+            tag=self.user_environment.tag
+        ).values_list('name', flat=True)
+        formatted_task_names = "{}".format("\n".join(tasks))
+        return formatted_task_names
+
+    def list_completed(self):
+        tasks = Task.objets.filter(
+            student=self.user,
+            state=Task.COMPLETED,
+            assessment=self.user_environment.assessment,
+            tag=self.user_environment.tag
+        ).values_list('name', flat=True)
+        formatted_task_names = "{}".format("\n".join(tasks))
+        return formatted_task_names
