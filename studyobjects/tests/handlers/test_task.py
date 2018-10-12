df30f570902bee35704b7dde49c94c81429fae8f
praveena <chvpraveena11@gmail.com>
2018-10-12 18:09:59 +0530

    assessment tests

--- studyobjects/tests/handlers/test_task.py
+++ studyobjects/tests/handlers/test_task.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from studyobjects.factory import UserEnvironmentFactory, CourseFactory, AssessmentFactory, TagFactory
 from studyobjects.handlers import TaskHandler
 from user.factory import TeamFactory, UserFactory, PlatformUserFactory, TeamMembershipFactory
 
@@ -10,7 +11,7 @@ class TaskTests(TestCase):
         team = TeamFactory(identity="T1345")
         user = UserFactory()
         platform_user = PlatformUserFactory(user=user)
-        TeamMembershipFactory(platform_user=platform_user, team=team)
+        tm = TeamMembershipFactory(platform_user=platform_user, team=team)
         data = {
             "name": "work on geometry",
             "eta": {
@@ -18,21 +19,9 @@ class TaskTests(TestCase):
                 "unit": 'h'
             }
         }
-        TaskHandler(data, platform_user, "create")
-
-
-
-    # formatted_task_name = slugify(self.response["name"])
-    # eta = get_displaced_time_from_duration_entity(self.response["eta"])
-    # team = self.user.team
-    # user_environment = UserEnvironment.objects.get(user=self.user)
-    # assessment = user_environment.assessment
-    # tag = user_environment.tag
-    # Task.objects.get_or_create(
-    #     name=formatted_task_name,
-    #     assessment=assessment,
-    #     tag=tag,
-    #     student=self.user,
-    #     eta=eta
-    # )
-    # return True
+        team = TeamFactory()
+        course = CourseFactory(team=team, instructor=tm)
+        assessment = AssessmentFactory(course=course, created_by=tm)
+        tag = TagFactory(course=course)
+        UserEnvironmentFactory(user=tm, tag=tag, assessment=assessment)
+        TaskHandler(data, tm, "create")
